91b09cec3d834d1aa6fb6fc4786c4cff04f7bab2
Siddhant <115331356+grimsteel@users.noreply.github.com>
2024-08-29 19:54:13 -0500

    improve error messages
    - handle ConnectTimeoutException
    - show response body in error messages

--- app/src/main/java/com/grimsteel/clearpasswifi/onboard/NetworkOnboard.kt
+++ app/src/main/java/com/grimsteel/clearpasswifi/onboard/NetworkOnboard.kt
@@ -6,12 +6,13 @@ import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
-import org.json.JSONArray
 import org.json.JSONObject
 import java.io.InputStream
+import java.net.ConnectException
 import java.net.URL
 import java.util.Date
 
@@ -24,15 +25,7 @@ suspend fun getCredentials(onboardUrl: URL, onboardOtp: String): InputStream {
     // prepare the request body
     val body = JSONObject()
     body.put("timestamp", Date().time / 1000)
-    body.put("auth_username", null)
-    body.put("auth_password", null)
-    body.put("mac_address", "02:00:00:00:00:00")
-    val interfaces = JSONArray()
-    interfaces.put(JSONObject())
-    body.put("network_interfaces", interfaces)
     body.put("device_type", "Android")
-    body.put("product_name", "Better Clearpass")
-    body.put("product_version", 2)
     body.put("otp", onboardOtp)
     body.put("certificate", 1)
 
@@ -47,12 +40,19 @@ suspend fun getCredentials(onboardUrl: URL, onboardOtp: String): InputStream {
         // check the response status code
         if (response.status != HttpStatusCode.OK) {
             Log.e("NetworkOnboard", "Status code is ${response.status}")
-            throw OnboardError("Onboarding server returned an error: ${response.status}", null)
+            val text = response.bodyAsText()
+            throw OnboardError(
+                "Onboarding server returned an error: ${response.status} - (\"$text\")",
+                null
+            )
         }
 
         return response.body()
     } catch (e: RuntimeException) {
         Log.e("NetworkOnboard", "Runtime exception while fetching data: $e")
+        throw OnboardError("${e.javaClass.simpleName}: ${e.message}", e)
+    } catch (e: ConnectException) {
+        Log.e("NetworkOnboard", "Connect exception while fetching data: $e")
         throw OnboardError("${e.javaClass.simpleName}: ${e.message}", e)
     }
 }
