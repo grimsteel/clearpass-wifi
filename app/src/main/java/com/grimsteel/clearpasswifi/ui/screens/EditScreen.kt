0de9abbe1eaf849f0578841fa18e3944c688c0c7
Siddhant <115331356+grimsteel@users.noreply.github.com>
2024-08-31 21:59:59 -0500

    finish edit screen

--- app/src/main/java/com/grimsteel/clearpasswifi/ui/screens/EditScreen.kt
+++ app/src/main/java/com/grimsteel/clearpasswifi/ui/screens/EditScreen.kt
@@ -1,22 +1,29 @@
 package com.grimsteel.clearpasswifi.ui.screens
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.net.wifi.WifiEnterpriseConfig
+import android.net.wifi.WifiManager
+import android.net.wifi.WifiNetworkSuggestion
+import android.security.keystore.KeyProperties
+import android.security.keystore.KeyProtection
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Edit
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -36,14 +43,24 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.grimsteel.clearpasswifi.R
+import com.grimsteel.clearpasswifi.data.Network
+import com.grimsteel.clearpasswifi.data.WpaMethod
 import com.grimsteel.clearpasswifi.ui.MainViewModelProvider
+import java.security.KeyStore
+import java.security.KeyStore.PrivateKeyEntry
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import javax.security.auth.x500.X500Principal
 
 @Composable
 fun EditScreen(vm: EditViewModel = viewModel(factory = MainViewModelProvider.Factory)) {
+    val dateFormat = SimpleDateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM)
+
     val network by vm.network.collectAsState()
     val uiState by vm.uiState.collectAsState()
     val context = LocalContext.current
 
+    // edit display name modal
     if (uiState.showEditDisplayNameModal) {
         Dialog(onDismissRequest = { vm.closeDisplayNameModal(false) }) {
             Card(
@@ -90,12 +107,11 @@ fun EditScreen(vm: EditViewModel = viewModel(factory = MainViewModelProvider.Fac
         modifier = Modifier
             .padding(16.dp)
             .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         // network name + edit button
         Row(
             modifier = Modifier
-                .padding(0.dp, 0.dp, 0.dp, 8.dp)
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
@@ -117,6 +133,7 @@ fun EditScreen(vm: EditViewModel = viewModel(factory = MainViewModelProvider.Fac
             ElevatedCard(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .padding(0.dp, 8.dp)
             ) {
                 Row(
                     modifier = Modifier
@@ -152,6 +169,179 @@ fun EditScreen(vm: EditViewModel = viewModel(factory = MainViewModelProvider.Fac
                     }
                 }
             }
+        }
+
+        network?.createdAt?.let {
+            Text(
+                text = stringResource(R.string.created_on, ""),
+                style = MaterialTheme.typography.labelMedium
+            )
+            Text(
+                text = dateFormat.format(it)
+            )
+            HorizontalDivider()
+        }
+
+        network?.wpaMethod?.let {
+            // WPA method
+            Text(
+                text = stringResource(R.string.wpa_method),
+                style = MaterialTheme.typography.labelMedium
+            )
+            Text(
+                text = when (it) {
+                    WpaMethod.EapTls -> stringResource(R.string.eap_tls)
+                }
+            )
+            HorizontalDivider()
+        }
+
+        // Identity
+        network?.identity?.let {
+            Text(
+                text = stringResource(R.string.eap_identity),
+                style = MaterialTheme.typography.labelMedium
+            )
+            Text(
+                text = it
+            )
+            HorizontalDivider()
+        }
+
+        // CA cert
+        network?.caCertificate?.let {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(
+                        text = stringResource(R.string.ca_certificate),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                    val cn = it.subjectX500Principal.name
+                        .split(",")
+                        .map { it.split("=") }
+                        .find { it[0] == "CN" };
+
+                    Text(
+                        text = cn?.get(1) ?: stringResource(R.string.unknown_cn),
+                        modifier = Modifier.wrapContentHeight()
+                    )
+                }
+                IconButton(
+                    onClick = { /* TODO: download certificate */ }
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.round_download_24),
+                        contentDescription = stringResource(R.string.download)
+                    )
+                }
+            }
+            HorizontalDivider()
+        }
+
+        // Client cert
+        network?.clientCertificate?.let {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(
+                        text = stringResource(R.string.client_certificate),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+
+                    val cn = it.subjectX500Principal.name
+                        .split(",")
+                        .map { it.split("=") }
+                        .find { it[0] == "CN" }
+
+                    Text(
+                        text = cn?.get(1) ?: stringResource(R.string.unknown_cn)
+                    )
+                }
+                IconButton(
+                    onClick = { /* TODO: download certificate */ }
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.round_download_24),
+                        contentDescription = stringResource(R.string.download)
+                    )
+                }
+            }
+            HorizontalDivider()
+
+            // private key
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(
+                        text = stringResource(R.string.private_key),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                    Text(
+                        text = stringResource(R.string.secure_device_storage),
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                }
+                IconButton(
+                    onClick = { /* TODO: download certificate */ }
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.round_download_24),
+                        contentDescription = stringResource(R.string.download)
+                    )
+                }
+            }
+            HorizontalDivider()
+        }
+
+        // button to add network
+        Button(onClick = {
+            network?.let {
+                val keyStore = KeyStore.getInstance("AndroidKeyStore").apply {
+                    load(null)
+                }
+                val pkEntry = keyStore.getEntry(
+                    Network.EAP_TLS_PK_ALIAS.format(it.id),
+                    // TODO: use actual protection
+                    null
+                ) as? PrivateKeyEntry
+
+                val pk = pkEntry?.privateKey
+
+                if (pk != null && it.clientCertificate != null) {
+                    val eapConfig = WifiEnterpriseConfig().apply {
+                        identity = it.identity
+                        domainSuffixMatch = it.domainSuffixMatch
+                        caCertificate = it.caCertificate
+                        eapMethod = when (it.wpaMethod) {
+                            WpaMethod.EapTls -> WifiEnterpriseConfig.Eap.TLS
+                        }
+                        setClientKeyEntry(pk, it.clientCertificate)
+                    }
+                    val suggestion = WifiNetworkSuggestion.Builder()
+                        .setSsid(it.ssid)
+                        .setMacRandomizationSetting(WifiNetworkSuggestion.RANDOMIZATION_PERSISTENT)
+                        .setWpa2EnterpriseConfig(eapConfig)
+                        .build()
+
+                    val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+                    wifiManager.addNetworkSuggestions(listOf(suggestion))
+                }
+            }
+        }) {
+            Text(text = "Add Wi-Fi network suggestion")
+        }
+        FilledTonalButton(onClick = { /*TODO*/ }) {
+            Text(text = "Add to your Wi-Fi networks")
         }
     }
 }
