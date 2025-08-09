0764c0923c22fc5e95afa56f824624590aaa7f76
Siddhant Kameswar <grimsteel@users.noreply.github.com>
2025-08-09 12:22:49 -0500

    v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.grimsteel.clearpasswifi"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
